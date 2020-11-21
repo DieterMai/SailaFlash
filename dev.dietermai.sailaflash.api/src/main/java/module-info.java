16afd8c5c096a0a451c78ba7b7da32c47cf441b4
Dieter Mai <maidieter@googlemail.com>
2020-11-21 11:20:46 +0100

    Switch to classpath

--- dev.dietermai.sailaflash.api/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-open module dev.dietermai.sailaflash.api {
-	exports dev.dietermai.sailaflash.api.bl;
-	exports dev.dietermai.sailaflash.api.model;
-	exports dev.dietermai.sailaflash.api.service;
-}
--- dev.dietermai.sailaflash.persistence.xml/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module dev.dietermai.sailaflash.persistence.xml {
-	exports dev.dietermai.sailaflash.persistence.xml;
-	exports dev.dietermai.sailaflash.persistence.xml.node;
-
-	requires dev.dietermai.sailaflash.api;
-	requires java.xml;
-	requires java.xml.bind;
-}
