6859d2b160c2dd0fc47653f1f5c2ddb265b9542d
Dieter Mai <maidieter@googlemail.com>
2020-12-22 15:13:26 +0100

    fix module issue

--- sailaflash.ui.jfx/src/main/java/module-info.java
+++ sailaflash.ui.jfx/src/main/java/module-info.java
@@ -3,7 +3,9 @@ module dev.dietermai.sailaflash.ui.jfx {
 	requires dev.dietermai.sailaflash.api;
 	requires javafx.base;
 	requires javafx.controls;
-	requires javafx.graphics;
+	requires transitive javafx.graphics;
+	
+	exports dev.dietermai.sailaflash.ui.jfx.app to javafx.graphics;
 	
 	provides dev.dietermai.sailaflash.api.service.IUiService with dev.dietermai.sailaflash.ui.jfx.service.JavaFxUiService;
 }
